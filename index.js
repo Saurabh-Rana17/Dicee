ad59d779925dd35f897c628c7ba9d0a1c3763104
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-15 23:54:40 +0530

    randomImg

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-var a = Math.random() * 6;
-a = Math.floor(a)+1;
-document.querySelector(".img1").setAttribute("src","images/dice1.png");
+var randomInt1 = Math.random() * 6;
+randomInt1 = Math.floor(randomInt1) + 1;
+var dicelist = ["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
+var randomImg = dicelist[randomInt1];
+document.querySelector(".img1").setAttribute("src", "randomImg");
